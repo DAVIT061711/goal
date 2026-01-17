cd151690b096edd06bca58e6fba9bffda8381d64
DAVIT061711 <dtsiklauri2011@gmail.com>
2026-01-16 22:46:07 -0800

    second commit

---
+++ day15/classwork/cw5.py
@@ -0,0 +1,11 @@
+#5)მომხმარებელს შემოატანინე მისი საყვარელი ფილმი და სახელი,
+# თუ მისი სახელი ავთოა მაშინ დაპრინტე "you are avto" , თუ ლევანი ქვია და მისი საყვარელი ფილმია ტიტანიკი, დაპრინტე 
+#"Levani loves titanic" ყველა სხვა შემთხვევაში დაპრინტე "someone likes other film" (indented if-else)
+
+user_name = input("enter your name")
+favorite_film= input("enter your favorite film")
+
+if user_name =="GIORGI":
+    print("you are GIORGI")
+elif user_name =="DAVITI" and favorite_film =="TRANSFORMERS":
+    print("DAVITI LOVES TRANSFORMERS")
