a2d7db5e971d608a3e750c35de44e4d83acc0897
DAVIT061711 <dtsiklauri2011@gmail.com>
2026-02-26 06:40:40 -0800

    second commit

---
+++ day21/homework/hw2.py
@@ -0,0 +1,29 @@
+#2)დაწერეთ ყველა ფუნქციაზე 3-3 მაგალითი
+
+print("hello")
+print(2026)
+print(5.5)
+
+print(len("davit"))
+print(len([10,20,230,0]))
+print(len("nika,gio,andria,nikolozi"))
+
+name=input("enter name")
+print(name)
+age=input("enter age")
+print(age)
+surname=input("enter surname")
+print(surname)
+
+print(type("50"))
+print(type(100))
+print(type(500))
+
+num1=int("5")
+print(num1)
+
+num1=float(5)
+print(num1)
+
+num1=str(5)
+print(num1)
