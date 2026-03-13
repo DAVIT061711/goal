8b93584bb66ef714788036657a711cae770209bb
DAVIT061711 <dtsiklauri2011@gmail.com>
2026-03-13 10:24:43 -0700

    second commit

--- day23/homework/hw5.py
+++ day23/homework/hw5.py
@@ -1,3 +1,14 @@
 #5)მომხმარებელს შემოატანინე მისი საყვარელი კინო და რაიმე ასო, დაპრინტე True თუ ეს ასო ამ კინოს სახელშია, თუ არადა დაპრინტე False
-#?
+
+
+
+film=input("enter your film")
+latter=input("enter latter")
+
+for i in film:
+    if i ==latter:
+        print(True)
+    else:
+        print(False)
+    
 
