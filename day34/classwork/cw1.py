dc18c2a49f394ba8fe5e1044014266bf11589b1d
DAVIT061711 <dtsiklauri2011@gmail.com>
2026-06-05 04:41:54 -0700

    s

---
+++ day34/classwork/cw1.py
@@ -0,0 +1,3 @@
+#1)შექმენი sports tuple რომელშიც შეინახავ 5 ელემენტს
+sports=("mma","wrestling","football","basketball","rygby")
+print(sports)
---
+++ day34/homework/hw3.py
@@ -0,0 +1,6 @@
+#3)შექმენი tuple და ჩაამატე მასში ელემენტი
+my_tuple2 = (1, 2, 3)
+temp_list = list(my_tuple2)
+temp_list.append(4)
+my_tuple2 = tuple(temp_list)
+
